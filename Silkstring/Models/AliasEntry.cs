27cd3f43c2fcf7be322cec5563037e5b529f439a
devoreofox <232652342+devoreofox@users.noreply.github.com>
2026-05-09 15:08:03 -0300

    Added AliasEntry model

---
+++ Silkstring/Models/AliasEntry.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Silkstring.Models;
+
+public class AliasEntry
+{
+    public static readonly string[] Blacklist = ["silkstring", "xlplugins", "xlsettings", "xldclose", "xldev"];
+
+    public bool Enabled = true;
+    public string Name = string.Empty;
+    public List<string> Output = new();
+
+    [NonSerialized]
+    public bool Delete;
+
+    [NonSerialized]
+    public int UniqueId;
+
+    public bool IsValid()
+    {
+        if (string.IsNullOrWhiteSpace(Name)) return false;
+        if (Blacklist.Contains(Name)) return false;
+        if (Name.Contains(' ')) return false;
+        if (Output.Count  == 0) return false;
+        return !Output.Any(command => string.IsNullOrWhiteSpace(command));
+    }
+}
